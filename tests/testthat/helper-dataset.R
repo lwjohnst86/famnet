35c01fdadee6352ab89d38b51ee733799d3ba90d
Luke W Johnston <lwjohnst@gmail.com>
2018-03-07 14:57:51 +0100

    update helper testthat file

--- tests/testthat/helper-dataset.R
+++ tests/testthat/helper-dataset.R
@@ -1,13 +1,14 @@
 
 # Load in test data 1, along with expected output
-testdata1 <- read.csv("testdata1.csv", stringsAsFactors = FALSE)
+devtools::load_data()
+testdata1 <- fmn_df
 testdata1_mother <-
     read.csv("testdata1-expected-mother.csv",
-             stringsAsFactors = FALSE, na.strings = "")
+             stringsAsFactors = FALSE, na.strings = "")[1:3]
 testdata1_father <-
     read.csv("testdata1-expected-father.csv",
-             stringsAsFactors = FALSE, na.strings = "")
+             stringsAsFactors = FALSE, na.strings = "")[1:3]
 
 # Load in test data 2
 # TODO: Add expected output data files
-testdata1 <- read.csv("testdata1.csv", stringsAsFactors = FALSE)
+testdata2 <- read.csv("testdata2.csv", stringsAsFactors = FALSE)

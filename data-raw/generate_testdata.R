8cba2fd1c47037dcec382d484d0364e5eaadaa71
Luke W Johnston <lwjohnst@gmail.com>
2018-06-18 15:36:48 +0200

    fmn_df as tibble [skip ci]

--- data-raw/generate_testdata.R
+++ data-raw/generate_testdata.R
@@ -1,4 +1,5 @@
 
 # Generate a fake/play dataset of a family network.
 fmn_df <- read.csv("data-raw/testdata1.csv", stringsAsFactors = FALSE)
+fmn_df <- dplyr::as_tibble(fmn_df)
 devtools::use_data(fmn_df, overwrite = TRUE)

28dce168a94fa2b708c183f4df9d9fdba8409a9a
Luke W Johnston <lwjohnst@gmail.com>
2018-06-18 14:48:01 +0200

    documenting fmn_df dataset

--- R/famnet-package.R
+++ R/famnet-package.R
@@ -3,3 +3,18 @@
 #'   everything
 #' @keywords internal
 "_PACKAGE"
+
+#' IDs for persons and their mother and/or father.
+#'
+#' A dataset that contains a person's ID, as well as ID's for their mother
+#' and/or father. This dataset is used to show how the famnet functions work.
+#'
+#' @format Dataframe with 50 rows and 4 variables:
+#' * PersonID: Person's ID
+#' * MotherID: Person's mother's ID
+#' * PersonID: Person's father's ID
+#' * Sex: Sex of the person
+#'
+"fmn_df"
+
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("."))
